c77b5b242d1c6d2458106dfbe73aef44cf4caa02
Ross Hanson <ross.hanson@gmail.com>
2014-11-05 00:13:53 -0500

    Added basic prisoner ids

--- PrisonerExpress/models.py
+++ PrisonerExpress/models.py
@@ -1,61 +1,59 @@
 from django.db import models
 from datetime import datetime
 
-# Create your models here.
 
 class Program(models.Model):
     name=models.CharField(max_length=200)
     active = models.BooleanField(default=True)
     continuous = models.BooleanField(default = True)
-    description = models.CharField(max_length=1000, default="N/A")  
+    description = models.CharField(max_length=1000, default="N/A")
     def __str__(self):
-            return self.name
+        return self.name
 
 class Prison(models.Model):
-        name=models.CharField(max_length=200)
-        address=models.CharField(max_length=200)
-        rules=models.CharField(max_length=1000)
-        
-        def __str__(self):
-                return self.name
+    name=models.CharField(max_length=200)
+    address=models.CharField(max_length=200)
+    rules=models.CharField(max_length=1000)
 
-        
+    def __str__(self):
+        return self.name
+
+
 class Prisoner(models.Model):
-	name=models.CharField(max_length=200)
-        active=models.BooleanField(default=True)
-        prison=models.ForeignKey(Prison, null=True)
-        programs=models.ManyToManyField(Program, related_name = "prisoners")
-        address=models.CharField(max_length=200,default="")
-	age=models.IntegerField()
-        last_active=models.DateTimeField('last active date', default=datetime.now)
+    name=models.CharField(max_length=100)
+    prisoner_id=models.CharField(max_length=80) #santitized version of the id
+    prisoner_id_raw=models.CharField(max_length=100) #how the id was entered
+    active=models.BooleanField(default=True)
+    prison=models.ForeignKey(Prison, null=True)
+    programs=models.ManyToManyField(Program, related_name = "prisoners")
+    address=models.CharField(max_length=200,default="")
+    age=models.IntegerField()
+    last_active=models.DateTimeField('last active date', default=datetime.now)
 
-        def __str__(self):
-                return self.name
+    def __str__(self):
+        return self.name
 
-        
+
 class Material(models.Model):
-        name=models.CharField(max_length=200)
-        program=models.ForeignKey(Program, related_name = "materials")
-        MATERIAL_TYPE_CHOICES = (
-                ('BO', 'Book'),
-                ('MA', 'Magazine'),
-                ('MO', 'Movie'),
-                ('TV', 'TV Show'),
-                ('PO', 'Poem'),
-                ('SS', 'Short Story'),
-                ('PH', 'Word or Phrase'),
-                ('PI', 'Picture'),
-                ('OO', 'Other')
-                )
-        material_type=models.CharField(max_length=2,
-                                       choices=MATERIAL_TYPE_CHOICES,
-                                       default='BO')                        
+    name=models.CharField(max_length=200)
+    program=models.ForeignKey(Program, related_name = "materials")
+    MATERIAL_TYPE_CHOICES = (
+        ('BO', 'Book'),
+        ('MA', 'Magazine'),
+        ('MO', 'Movie'),
+        ('TV', 'TV Show'),
+        ('PO', 'Poem'),
+        ('SS', 'Short Story'),
+        ('PH', 'Word or Phrase'),
+        ('PI', 'Picture'),
+        ('OO', 'Other')
+        )
+    material_type=models.CharField(max_length=2,
+                                   choices=MATERIAL_TYPE_CHOICES,
+                                   default='BO')
 
-        
+
 class Letter(models.Model):
-        prisoner=models.ForeignKey(Prisoner)
-        content=models.TextField()
-        program=models.ForeignKey(Program, related_name = "letters")
-        
-
-        
+    prisoner=models.ForeignKey(Prisoner)
+    content=models.TextField()
+    program=models.ForeignKey(Program, related_name = "letters")
--- PrisonerExpress/prisoner_views.py
+++ PrisonerExpress/prisoner_views.py
@@ -4,51 +4,70 @@ from django.views.generic import ListView, DetailView, TemplateView
 from PrisonerExpress.models import Prison, Prisoner
 from django.shortcuts import get_object_or_404, render, redirect
 
+
 def index(request):
     total = len(Prison.objects.all())
     return HttpResponse("There are %s prisoners in the system" % total)
 
+
 def details(request, prisoner_id):
     p = Prisoner.objects.get(pk=prisoner_id)
     return HttpResponse("%s has prisoner id of %s" % (p.name, p.id))
-    
-def create_prisoner(prisoner_name, prisoner_address, prison_id, rules):
+
+
+def create_prisoner(prisoner_name, prisoner_id,
+                    prisoner_address, prison_id, rules):
     if prisoner_name is None:
         raise Exception("Prisoner must have a name")
+    if prisoner_id is None:
+        raise Exception("Prisoner must have an address")
     if prisoner_address is None and prison_id == -1:
-        raise Exception("Prisoner Address and Prison cannot both be unspecified")
+        raise Exception(
+            "Prisoner Address and Prison cannot both be unspecified")
+    sanitized_id = filter(str.isalnum, prisoner_id)
     prison = None
     if int(prison_id) != -1:
         prison = Prison.objects.get(pk=prison_id)
     address = prisoner_address if len(prisoner_address) > 0 else prison.address
     p = Prisoner(name=prisoner_name,
+                 prisoner_id = sanitized_id,
+                 prisoner_id_raw = prisoner_id,
                  active=True,
                  prison=prison,
                  address=address,
                  age=-1)
     p.save()
-    return p.id    
+    return p.id
+
 
 def get_letters(request, prisoner_id):
     prisoner = Prisoner.objects.get(pk=prisoner_id)
     if request.GET['groupby'] == None:
         return Letters.objects.get(prisoner=prisoner)
-        
-        
+
+
 def create(request):
     if request.method == 'POST':
-        new_p = create_prisoner(request.POST['name'], request.POST['mailing_address'],
-                                request.POST['prison'], request.POST['prison_rules'])
+        new_p = create_prisoner(request.POST['name'],
+                                request.POST['prisoner_id'],
+                                request.POST['mailing_address'],
+                                request.POST['prison'],
+                                request.POST['prison_rules'])
         return redirect('prisoner_details', pk=new_p)
-    return render(request, "create_prisoner.html", {'prison_list':Prison.objects.all()})
+    return render(request,
+                  "create_prisoner.html",
+                  {'prison_list':Prison.objects.all()})
+
 
 class PrisonerList(ListView):
     template_name='prisoner_list.html'
     model=Prisoner
 
+
 class PrisonerDetail(DetailView):
     template_name="prisoner_detail.html"
     model=Prisoner
+
 
 class PrisonerIndex(TemplateView):
     template_name="prisoner_index.html"
