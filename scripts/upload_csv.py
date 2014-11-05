29fa57a4f29f93a137c37d114f29e51c604d8766
Ross Hanson <ross.hanson@gmail.com>
2014-11-05 13:48:07 -0500

    Minor fixes

--- scripts/upload_csv.py
+++ scripts/upload_csv.py
@@ -13,13 +13,14 @@ def read_file(fname):
             if len(row) != len(header):
                 continue
             prisoner_id_sanitized = pattern.sub('',row[header['PrisonerID']])
+            print "ID: %s " % prisoner_id_sanitized
             a = Address(city=row[header['City']],
                         state=row[header['State']],
                         postal_code=row[header['ZipCode']],
                         address_1=row[header['PreAddress']],
                         address_2=row[header['Address']])
             a.save()
-            p = Prisoner(name=row[header['FirstName']] +
+            p = Prisoner(name=row[header['FirstName']] +' '+
                                  row[header['LastName']],
                          prisoner_id_raw=row[header['PrisonerID']],
                          prisoner_id=prisoner_id_sanitized,
