c51ed984de5867d7899463858a376524a011673b
Ross Hanson <ross.hanson@gmail.com>
2014-11-05 02:01:41 -0500

    Fixed up basic address stuff

--- PrisonerExpress/models.py
+++ PrisonerExpress/models.py
@@ -13,22 +13,34 @@ class Program(models.Model):
 
 class Address(models.Model):
     """Class to represent all addresses, thus standardizing address handling code"""
-    locality = models.CharField(max_length=100) #City
-    region = models.CharField(max_length=100) #State
+    city = models.CharField(max_length=100) #City
+    state = models.CharField(max_length=100) #State
     postal_code = models.CharField(max_length=10) #Room for zip-plus-4
-    street_address= models.CharField(max_length=100)
+    address_1= models.CharField(max_length=100) #Line 1
+    address_2= models.CharField(max_length=100) #Line 2
+    address_3= models.CharField(max_length=100) #Line 3
     raw = models.CharField(max_length=200, default="")
 
-    
+    def get_lines(self):
+        addr = []
+        if (len(self.address_1) >0 ):
+            addr.append(self.address_1)
+        if (len(self.address_2) >0):
+            addr.append(self.address_2)
+        if (len(self.address_3) >0):
+            addr.append(self.address_3)
+        addr.append("%s, %s %s" % (self.city, self.state, self.postal_code))
+        return addr
+
 class Prison(models.Model):
     name=models.CharField(max_length=200)
     primary_address=models.ForeignKey(Address)
     rules=models.CharField(max_length=1000)
-    
+
     def __str__(self):
         return self.name
 
-        
+
 class Prisoner(models.Model):
     name=models.CharField(max_length=200)
     prisoner_id=models.CharField(max_length=80) #santitized version of the id
@@ -42,7 +54,7 @@ class Prisoner(models.Model):
     def __str__(self):
         return "Name: %s | ID: %s " % (self.name, self.prisoner_id)
 
-    
+
 class Material(models.Model):
     name=models.CharField(max_length=200)
     program=models.ForeignKey(Program, related_name = "materials")
--- PrisonerExpress/tests.py
+++ PrisonerExpress/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from PrisonerExpress.models import Prison, Prisoner
+from PrisonerExpress.models import Prison, Prisoner, Address
 from django.http import HttpResponse
 # Create your tests here.
 
@@ -11,15 +11,17 @@ def input(request):
         city=request.GET['city'];
         state=request.GET['state'];
         zipcode=request.GET['zipcode'];
+        a = Address(city=city, state=state,
+                    postal_code=str(int(zipcode)),
+                    address_1=pre_address,
+                    address_2=address)
+        a.save()
         raw_id=request.GET['id'];
         parsed_id = filter(str.isalnum, str(raw_id))
         p = Prisoner(name=first_name+' '+last_name,
-                 active=True,
-                 address=address,
-#                 city=city,
-#                 state=state,
-#                 zipcode=zipcode,
+                     active=True,
+                     address=a,
                      prisoner_id=parsed_id,
-                     prisoner_id_raw = parsed_id)
+                     prisoner_id_raw = raw_id)
         p.save()
         return HttpResponse("%s has prisoner id of %s" % (p.name, p.id))
